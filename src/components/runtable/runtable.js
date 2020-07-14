95937dd5fb225225548cd194e15e1c4e28576fd8
mrfleming <ericfleming8@gmail.com>
2020-07-14 15:05:43 -0500

    deleted comments, old code

--- src/components/runtable/runtable.js
+++ src/components/runtable/runtable.js
@@ -56,20 +56,3 @@ const Runtable = () => {
 }
 
 export default Runtable
-
-/*
-
-{runs.map(run => (
-                    <RunRow
-                        index={run.index}
-                        date={run.date}
-                        dist={run.dist}
-                        time={run.time}
-                        pace={run.pace}
-                    />
-                ))}
-<RunRow index="1" date="6/11/2020" dist="5.40 mi" time="0:44:23" pace="8:11" />
-                <RunRow index="2" date="6/10/2020" dist="5.35 mi" time="0:43:55" pace="8:35" />
-                <RunRow index="3" date="6/09/2020" dist="5.39 mi" time="0:42:03" pace="8:23" />
-                <RunRow index="4" date="6/07/2020" dist="5.40 mi" time="0:46:17" pace="8:42" />
-*/
