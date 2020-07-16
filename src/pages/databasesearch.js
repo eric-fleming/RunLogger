c2b3d95c161dfc65d36cc960eca7bad56aefdfe0
mrfleming <ericfleming8@gmail.com>
2020-07-16 16:33:42 -0500

    aggregated other components

--- src/pages/databasesearch.js
+++ src/pages/databasesearch.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import TestDB from '../components/testdb/testdb';
-
+import GetRunners from '../components/testdb/getRunners';
+import GetRuns from '../components/testdb/getRuns';
 
 function DatabaseSearch() {
     return (
-        <div>
-            <TestDB />
-        </div>
+        <section>
+            <h2>Test DB Output</h2>
+            <div>
+                <GetRunners />
+            </div>
+            <div>
+                <GetRuns />
+            </div>
+        </section>
     )
 }
 
-export default DatabaseSearch
+export default DatabaseSearch;
