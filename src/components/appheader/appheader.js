cdb5d5c29856ec96432f493466b6ee533b411943
mrfleming <ericfleming8@gmail.com>
2020-07-16 14:52:58 -0500

    moving app header to its own component

---
+++ src/components/appheader/appheader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './appheader.module.css';
+
+
+function AppHeader() {
+    return (
+        <header className="App-header">
+            <h1>RunLogger</h1>
+        </header>
+    )
+}
+
+export default AppHeader
