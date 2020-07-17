485b08a8d07e69eeee1349572aa3316c88f554a7
mrfleming <ericfleming8@gmail.com>
2020-07-17 17:44:24 -0500

    moved header inside app tag

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { Route, Switch, Redirect, BrowserRouter as Router } from 'react-router-dom';
 
 // Components
+import AppHeader from './components/appheader/appheader';
 import MainPage from './pages/mainpage';
 import PageNotFound from './pages/pagenotfound';
 import DatabaseSearch from './pages/databasesearch';
@@ -16,17 +17,21 @@ function App() {
   
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={MainPage} />
-        <Route exact path="/database" component={DatabaseSearch} />
-        <Route exact path="/runners" component={GetRunners} />
-        <Route exact path="/runs" component={GetRuns} />
-        <Route exact path="/404" component={PageNotFound} />
-        <Redirect to="/404" />
-      </Switch>
-      
-    </Router>
+    <div>
+      <Router>
+        <AppHeader />
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/database" component={DatabaseSearch} />
+          <Route exact path="/runners" component={GetRunners} />
+          <Route exact path="/runs" component={GetRuns} />
+          <Route exact path="/404" component={PageNotFound} />
+          <Redirect to="/404" />
+        </Switch>
+
+      </Router>
+    </div>
+    
   );
 }
 
