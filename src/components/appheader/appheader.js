7fba4c13b443d5274177fbfc3128432c612be545
mrfleming <ericfleming8@gmail.com>
2020-07-17 17:45:04 -0500

    configured new app navigation

--- src/components/appheader/appheader.js
+++ src/components/appheader/appheader.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import './appheader.module.css';
-
+import styles from './appheader.module.css';
+import { Link } from 'react-router-dom';
 
 function AppHeader() {
     return (
-        <header className="App-header">
+        <header className={styles.App_header}>
             <h1>RunLogger</h1>
+            <div class={styles.nav_menu}>
+                <div className={styles.nav_item}>
+                    <Link className={styles.nav_text} to="/">Home</Link>
+                </div>
+                <div className={styles.nav_item}>
+                    <Link className={styles.nav_text} to="/database">Database</Link>
+                </div>
+                <div className={styles.nav_item}>
+                    <Link className={styles.nav_text} to="/runners">Runners</Link>
+                </div>
+                <div className={styles.nav_item}>
+                    <Link className={styles.nav_text} to="/runs">History</Link>
+                </div>
+            </div>
         </header>
     )
 }
