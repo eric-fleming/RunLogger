6233f692684b6d9361e3832c9f9ec472b5a4fb17
mrfleming <ericfleming8@gmail.com>
2020-08-12 14:51:26 -0500

    enabling production

---
+++ build/precache-manifest.bc4f6d12b0bc6491bf8158b1aedc55fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec770027a895271a16428946ac73931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "483213c7e83d786bdc2b",
+    "url": "/static/css/main.5919f8bc.chunk.css"
+  },
+  {
+    "revision": "026785a8c32d390a6084",
+    "url": "/static/js/2.cefba3ff.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cefba3ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "483213c7e83d786bdc2b",
+    "url": "/static/js/main.7747a883.chunk.js"
+  },
+  {
+    "revision": "505e2d58d78dd7fe5ba5",
+    "url": "/static/js/runtime-main.7f47f227.js"
+  }
+]);
