49cba2ea601ed148fd1f523e7202682bb4d73ff4
mrfleming <ericfleming8@gmail.com>
2020-08-12 01:13:25 -0500

    removed appheader since new ui design covers it

--- src/components/appheader/appheader.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import AppHeader from './appheader';
-
-test('renders learn react link', () => {
-    const { getByText } = render(<AppHeader />);
-    const title = getByText(/runlogger/i);
-    expect(title).toBeInTheDocument();
-});
