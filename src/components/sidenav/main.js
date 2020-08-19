99bacb509388c96a256d63d9035db41fae58f20e
mrfleming <ericfleming8@gmail.com>
2020-08-19 11:32:45 -0500

    added new UI routes

--- src/components/sidenav/main.js
+++ src/components/sidenav/main.js
@@ -7,14 +7,12 @@ import { Route, Switch, Redirect} from 'react-router-dom';
 import MainPage from '../../pages/mainpage';
 import PageNotFound from '../../pages/pagenotfound';
 import DatabaseSearch from '../../pages/databasesearch';
-
-import Profile from '../profile/profile';
 import DetailedProfile from '../profile/detailedprofile';
 import GetRunners from '../runnersUI/getRunners';
 import GetRuns from '../runsUI/getRuns';
 import Feed from '../feed/feed';
-
-
+import Settings from '../../pages/settings';
+import Challenges from '../../pages/challenges';
 const Main = () => {
 
 
@@ -27,6 +25,8 @@ const Main = () => {
                 <Route exact path="/runners" component={GetRunners} />
                 <Route exact path="/runs" component={GetRuns} />
                 <Route exact path="/feed" component={Feed} />
+                <Route exact path="/challenges" component={Challenges} />
+                <Route exact path="/settings" component={Settings} />
                 <Route exact path="/404" component={PageNotFound} />
                 <Redirect to="/404" />
             </Switch>
